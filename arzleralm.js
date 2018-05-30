df8b5d4da439434c8d922392cef73fc5656d8a4a
sagamol <christian.hofer@student.uibk.ac.at>
2018-05-30 17:40:50 +0200

    Bildquellen Arzler Alm eingefügt

--- arzleralm.js
+++ arzleralm.js
@@ -153,7 +153,8 @@ L.marker([47.286434,11.398509],{
         popupAnchor : [0,-37],
     })
 }).addTo(overlayMarker).bindPopup(
-    '<h3>Hungerburg Parkplatz</h3><img src="images/hungerburgparkplatz.jpg"/>'
+    `<h3>Hungerburg Parkplatz</h3><img src="images/hungerburgparkplatz.jpg"/>
+    <p><a href="http://www.skiresort.de/typo3temp/_processed_/da/a0/05/5f/3160c11116.jpg">Bildquelle Foto</a></p>`
 );
 
 L.marker([47.296362,11.403082],{
@@ -163,7 +164,8 @@ L.marker([47.296362,11.403082],{
         popupAnchor : [0,-37],
     })
 }).bindPopup(
-    '<h3>Arzler Alm</h3><img src="images/arzleralm.jpg"/>'
+    `<h3>Arzler Alm</h3><img src="images/arzleralm.jpg"/>
+    <p><a href="http://www.arzleralm.at/data.cfm?vpath=teaserbilder/sommer-west2&dimensions=664x9999&cropmode=keepratio&noenlargement=no">Bildquelle Foto</a></p>`
 ).addTo(overlayMarker);
 
 // GeoJSON Track als Linie in der Karte einzeichnen und auf Ausschnitt zoomen
