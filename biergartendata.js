e6a9c30746fffd4657bc55f283a61a9638bcd1b5
sagamol <christian.hofer@student.uibk.ac.at>
2018-06-09 15:35:40 +0200

    daten verfeinert

--- biergartendata.js
+++ biergartendata.js
@@ -3,22 +3,22 @@ let biergartendata = [
       "lat":"47.263473","lng":"11.394978","name":"Theresienbraeu","adresse":"Maria_Theresien_Strasse 51-53","link":"http://www.theresienbraeu.com","bild":"images/theresienbraeu.jpg","bildquelle":"http://www.theresienbraeu.com/images/speasyimagegallery/albums/1/images/dsc01639.gif" 
     },
     {
-      "lat":"47.2767846","lng":"11.3991046","name":"Loewenhaus","adresse":"Rennweg 5","link": "http://www.loewenhaus.at","bild":"images/loewenhaus.jpg","bildquelle":"http://www.loewenhaus.at/wp-content/uploadsgallery/gastgarten/bild_04.jpg"
+      "lat":"47.275881","lng":"11.397966","name":"Loewenhaus","adresse":"Rennweg 5","link": "http://www.loewenhaus.at","bild":"images/loewenhaus.jpg","bildquelle":"http://www.loewenhaus.at/wp-content/uploadsgallery/gastgarten/bild_04.jpg"
     },
     {
-      "lat":"47.2697068","lng":"11.3974477","name":"Hofgarten","adresse":"Rennweg 6a","link":"http://www.hofgarten.cafe","bild":"images/hofgarten.jpg","bildquelle":"https://sperrstunde.at/media/1370/dsc-2.jpg?anchor=center&amp;mode=crop&amp;quality=90&amp;width=800&amp;heightratio=0.5&amp;format=jpg&amp;slimmage=true&amp;rnd=130918009150000000" 
+      "lat":"47.270704","lng":"11.398327","name":"Hofgarten","adresse":"Rennweg 6a","link":"http://www.hofgarten.cafe","bild":"images/hofgarten.jpg","bildquelle":"https://sperrstunde.at/media/1370/dsc-2.jpg?anchor=center&amp;mode=crop&amp;quality=90&amp;width=800&amp;heightratio=0.5&amp;format=jpg&amp;slimmage=true&amp;rnd=130918009150000000" 
     },
     {
       "lat":"47.265893","lng":"11.392105","name":"Goessers","adresse":"Adolf_Pichler_Platz 3","link":"http://www.goessers.at","bild":"images/goessers.jpg","bildquelle":"https://media-cdn.tripadvisor.com/media/photo-s/0c/cc/70/64/goessers.jpg" 
     },
     {
-      "lat":"47.2514265","lng":"11.4012491","name":"Bierstindl","adresse":"Klostergasse 6","link":"https://www.bierstindl.eu","bild":"images/bierstindl.jpg","bildquelle":"https://www.bing.com/th?id=OIP.IlzqoFW7vucsffzQZ3eFHQHaE8&pid=Api" 
+      "lat":"47.251861","lng":"11.401011","name":"Bierstindl","adresse":"Klostergasse 6","link":"https://www.bierstindl.eu","bild":"images/bierstindl.jpg","bildquelle":"https://www.bing.com/th?id=OIP.IlzqoFW7vucsffzQZ3eFHQHaE8&pid=Api" 
     },
     {
-      "lat":"47.279924","lng":"11.4041895","name":"Restaurant Panorama","adresse":"Rennweg 39","link":"http://restaurant-panorama.info","bild":"images/panorama.jpg","bildquelle":"https://all-inn.at/essen-und-trinken-gehen-in-innsbruck/biergarten-beergarden/restaurant-panorama/" 
+      "lat":"47.279584","lng":"11.403896","name":"Restaurant Panorama","adresse":"Rennweg 39","link":"http://restaurant-panorama.info","bild":"images/panorama.jpg","bildquelle":"https://all-inn.at/essen-und-trinken-gehen-in-innsbruck/biergarten-beergarden/restaurant-panorama/" 
     },
     {
-      "lat":"47.2685811","lng":"11.3936288","name":"","adresse":"Stiftskeller","link":"https://www.stiftskeller.eu/","bild":"images/stiftskeller.jpg","bildquelle":"https://www.innsbruck.info/emobilder/x/15131/Biergarten.jpg" 
+      "lat":"47.268441","lng":"11.394782","name":"Stiftskeller","adresse":"Stiftgasse 1","link":"https://www.stiftskeller.eu/","bild":"images/stiftskeller.jpg","bildquelle":"https://www.innsbruck.info/emobilder/x/15131/Biergarten.jpg" 
     },
     {
       "lat":"47.3063381","lng":"11.3797005","name":"Alpenlounge Seegrube","adresse":"","link":"https://www.nordkette.com/de/restaurant-seegrube.html","bild":"images/seegrube.jpg","bildquelle":"http://static2.bergfex.com/images/downsized/c0/6b1bf0eb4f4e75c0_5449ae654d5d4fe9.jpg" 
@@ -27,31 +27,31 @@ let biergartendata = [
       "lat":"47.2730955","lng":"11.3933872","name":"Gastgarten zur Eiche","adresse":"","link":"http://zureiche.at","bild":"images/eiche.jpg","bildquelle":"https://www.blog.tirol/wp-content/uploads/2016/08/ZurEiche-560x371.jpg" 
     },
     {
-      "lat":"47.2696596","lng":"11.3994036","name":"Kapuziner","adresse":"Kaiserjaegerstrasse 4a","link":"https://www.facebook.com/kapuziner6020","bild":"images/kapuziner.jpg","bildquelle":"https://www.innsbruck.info/emobilder/1000cx550c/15917/Gastgarten.jpg" 
+      "lat":"47.269704","lng":"11.399864","name":"Kapuziner","adresse":"Kaiserjaegerstrasse 4a","link":"https://www.facebook.com/kapuziner6020","bild":"images/kapuziner.jpg","bildquelle":"https://www.innsbruck.info/emobilder/1000cx550c/15917/Gastgarten.jpg" 
     },
     {
-      "lat":"47.2694643","lng":"11.3934397","name":"Fischerhaeusl","adresse":"Herrengasse 8","link":"http://www.fischerhaeusl.com","bild":"images/fischerhaeusl.jpg","bildquelle":"https://all-inn.at/essen-und-trinken-gehen-in-innsbruck/restaurants-in-innsbruck/osterreichische-restaurants/fischerhaeusl/" 
+      "lat":"47.269749","lng":"11.394431","name":"Fischerhaeusl","adresse":"Herrengasse 8","link":"http://www.fischerhaeusl.com","bild":"images/fischerhaeusl.jpg","bildquelle":"https://all-inn.at/essen-und-trinken-gehen-in-innsbruck/restaurants-in-innsbruck/osterreichische-restaurants/fischerhaeusl/" 
     },
     {
-      "lat":"47.2575182","lng":"11.427565","name":"Bierwirt","adresse":"Bichlweg 2","link":"http://www.bierwirt.com","bild":"images/bierwirt.jpg","bildquelle":"https://zvr-img.global.ssl.fastly.net/unsafe/trim/800x/images/entities/vrw/01210000-01219999/E1215067-foto466196.jpg" 
+      "lat":"47.258615","lng":"11.430995","name":"Bierwirt","adresse":"Bichlweg 2","link":"http://www.bierwirt.com","bild":"images/bierwirt.jpg","bildquelle":"https://zvr-img.global.ssl.fastly.net/unsafe/trim/800x/images/entities/vrw/01210000-01219999/E1215067-foto466196.jpg" 
     },
     {
-      "lat":"47.2490702","lng":"11.3675894","name":"Gasthaus Peterbruennl","adresse":"Voelser Strasse 25","link":"https://www.peterbruennl.at/","bild":"images/peterbruennl.jpg","bildquelle":"https://www.peterbruennl.at/bilder/" 
+      "lat":"47.251498","lng":"11.371222","name":"Gasthaus Peterbruennl","adresse":"Voelser Strasse 25","link":"https://www.peterbruennl.at/","bild":"images/peterbruennl.jpg","bildquelle":"https://www.peterbruennl.at/bilder/" 
     },
     {
-      "lat":"47.2518797","lng":"11.4048398","name":"Gasthaus Bretterkeller","adresse":"Viller Berg 1","link":"https://www.gasthaus-bretterkeller.at","bild":"images/bretterkeller.jpg","bildquelle":"https://www.innsbruck.info/it/infrastructures/dettagli/infrastruktur/restaurant-bretterkeller-innsbruck.html" 
+      "lat":"47.252532","lng":"11.405309","name":"Gasthaus Bretterkeller","adresse":"Viller Berg 1","link":"https://www.gasthaus-bretterkeller.at","bild":"images/bretterkeller.jpg","bildquelle":"https://www.innsbruck.info/it/infrastructures/dettagli/infrastruktur/restaurant-bretterkeller-innsbruck.html" 
     },
     {
-      "lat":"47.2641426","lng":"11.4258137","name":"Restaurant Martin","adresse":"Geyrstrasse 3","link":"http://www.cafe-restaurant-martin.at","bild":"images/cafemartin.jpg","bildquelle":"http://www.cafe-restaurant-martin.at/uploads/tx_jpcarousel/005_02.jpg" 
+      "lat":"47.263941","lng":"11.424370","name":"Restaurant Martin","adresse":"Geyrstrasse 3","link":"http://www.cafe-restaurant-martin.at","bild":"images/cafemartin.jpg","bildquelle":"http://www.cafe-restaurant-martin.at/uploads/tx_jpcarousel/005_02.jpg" 
     },
     {
-      "lat":"47.2555207","lng":"11.394446","name":"Gasthaus Riese Haymon","adresse":"Haymongasse 4","link":"https://www.riese-haymon.at/","bild":"images/riesehaymon.jpg","bildquelle":"https://www.innsbruck.info/it/infrastructures/dettagli/infrastruktur/gasthaus-riese-haymon-innsbruck.html" 
+      "lat":"47.255390","lng":"11.397458","name":"Gasthaus Riese Haymon","adresse":"Haymongasse 4","link":"https://www.riese-haymon.at/","bild":"images/riesehaymon.jpg","bildquelle":"https://www.innsbruck.info/it/infrastructures/dettagli/infrastruktur/gasthaus-riese-haymon-innsbruck.html" 
     },
     {
       "lat":"47.259905","lng":"11.396397","name":"Gasthaus Steneck","adresse":"Leopoldstrasse 21","link":"https://www.facebook.com/pages/Gasthaus-Steneck/188884881152417","bild":"images/steneck.jpg","bildquelle":"https://www.yelp.at/biz/steneck-innsbruck" 
     },
     {
-      "lat":"47.2622246","lng":"11.3947435","name":"Vapiano","adresse":"Leopoldstrasse 1","link":"https://at.vapiano.com/de/restaurants/vapiano-innsbruck-2-leopoldstrasse-1/","bild":"images/vapiano.jpg","bildquelle":"https://all-inn.at/wp-content/uploads/2014/08/vapiano-innsbruck-zentrum-1-800x445.jpg" 
+      "lat":"47.262380","lng":"11.395186","name":"Vapiano","adresse":"Leopoldstrasse 1","link":"https://at.vapiano.com/de/restaurants/vapiano-innsbruck-2-leopoldstrasse-1/","bild":"images/vapiano.jpg","bildquelle":"https://all-inn.at/wp-content/uploads/2014/08/vapiano-innsbruck-zentrum-1-800x445.jpg" 
     },
     {
       "lat":"47.2683536","lng":"11.3918077","name":"Ottoburg","adresse":"Herzog_Friedrich_Strasse 1","link":"http://www.ottoburg.at/de/","bild":"images/ottoburg.jpg","bildquelle":"https://all-inn.at/essen-und-trinken-gehen-in-innsbruck/restaurants-in-innsbruck/osterreichische-restaurants/ottoburg/" 
@@ -63,10 +63,10 @@ let biergartendata = [
       "lat":"47.268488","lng":"11.3926827","name":"Elferhaus","adresse":"Herzog_Friedrich_Strasse 11","link":"https://www.facebook.com/elferhaus","bild":"images/elferhaus.jpg","bildquelle":"https://all-inn.at/wp-content/gallery/bars-elferhaus/elferhaus-innsbruck-zentrum-2.jpg" 
     },
     {
-      "lat":"47.2626761","lng":"11.3967122","name":"Stieglbraeu","adresse":"Wilhelm_Greil_Strasse 25","link":"http://www.stiegl-braeu-innsbruck.at/","bild":"images/stieglbraeu.jpg","bildquelle":"http://www.stiegl-braeu-innsbruck.at/images/Imagebilder/Stiegl-Gastgarten3.jpg" 
+      "lat":"47.263240","lng":"11.396752","name":"Stieglbraeu","adresse":"Wilhelm_Greil_Strasse 25","link":"http://www.stiegl-braeu-innsbruck.at/","bild":"images/stieglbraeu.jpg","bildquelle":"http://www.stiegl-braeu-innsbruck.at/images/Imagebilder/Stiegl-Gastgarten3.jpg" 
     },
     {
-      "lat":"47.2711355","lng":"11.3732702","name":"Gasthaus Hoettinger Schießstand","adresse":"Schwabeneckweg 7","link":"https://www.facebook.com/gasthaus.hoettinger.schiessstand","bild":"images/schiessstand.jpg","bildquelle":"https://www.innsbruck.info/emobilder/1000cx550c/12663/Gasthaus-Schie%C3%9Fstand-Innsbruck.jpg" 
+      "lat":"47.271080","lng":"11.375440","name":"Gasthaus Hoettinger Schießstand","adresse":"Schwabeneckweg 7","link":"https://www.facebook.com/gasthaus.hoettinger.schiessstand","bild":"images/schiessstand.jpg","bildquelle":"https://www.innsbruck.info/emobilder/1000cx550c/12663/Gasthaus-Schie%C3%9Fstand-Innsbruck.jpg" 
     },
     {
       "lat":"47.2701405","lng":"11.358619","name":"Gasthaus Berchtoldshof","adresse":"Schneeburggasse 140","link":"https://www.berchtoldshof.com/","bild":"images/Berchtoldshof.jpg","bildquelle":"https://www.innsbruck.info/emobilder/x/27309/Berchtoldshof.jpg" 
@@ -78,7 +78,7 @@ let biergartendata = [
       "lat":"47.2748016","lng":"11.3758528","name":"Gasthaus Planötzenhof","adresse":"Planötzenhofstraße 30","link":"http://www.planoetzenhof.at","bild":"images/planoetzenhof.jpg","bildquelle":"http://www.planoetzenhof.at/uploads/media/planoetzenhof_aussen_02.jpg" 
     },
     {
-      "lat":"47.2774699","lng":"11.3866415","name":"Gasthof Oelberg","adresse":"Höhenstrasse 52","link":"http://www.oelberg.eu","bild":"images/oelberg.jpg","bildquelle":"https://media-cdn.tripadvisor.com/media/photo-s/08/84/af/0a/olberg.jpg" 
+      "lat":"47.278974","lng":"11.389443","name":"Gasthof Oelberg","adresse":"Höhenstrasse 52","link":"http://www.oelberg.eu","bild":"images/oelberg.jpg","bildquelle":"https://media-cdn.tripadvisor.com/media/photo-s/08/84/af/0a/olberg.jpg" 
     },
     {
       "lat":"47.2835293","lng":"11.3794032","name":"Gasthof Gramarthof","adresse":"Gramartstraße 117","link":"http://www.gramarthof.com/","bild":"images/gramarthof.jpg","bildquelle":"http://www.gramarthof.com/site/assets/files/1/dscn2014_a9107ca92b.jpg" 
