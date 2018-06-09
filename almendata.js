4ebccb8ef56acd42d1924acb517dccb358758faf
sagamol <christian.hofer@student.uibk.ac.at>
2018-06-09 15:02:32 +0200

    Datenbank ausgebessert

--- almendata.js
+++ almendata.js
@@ -3,16 +3,16 @@ const almendata = [
       "lat":"47.2790279","lng":"11.347223","name":"Alpengasthof Rauschbrunnen","link":"https://www.innsbruck.info/infrastruktur/detail/infrastruktur/alpengasthof-rauschbrunnen-innsbruck.html","bild":"images/rauschbrunnen.jpg","bildquelle":"https://www.mountaineers.at/files/styles/mega/public/images/wanderung/rauschbrunnen/rauschbrunnenWandern-1.jpg?itok=yVoXVmsv" 
     },
     {
-      "lat":"47.2991919","lng":"11.3679229","name":"Höttinger Alm","link":"https://hoettingeralm.at","bild":"images/hoettingeralm.jpg","bildquelle":"https://www.almenrausch.at/uploads/tx_wctrip/Hoettingeralm_7748.jpg" 
+      "lat":"47.298466","lng":"11.366922","name":"Höttinger Alm","link":"https://hoettingeralm.at","bild":"images/hoettingeralm.jpg","bildquelle":"https://www.almenrausch.at/uploads/tx_wctrip/Hoettingeralm_7748.jpg" 
     },
     {
-      "lat":"47.2931336","lng":"11.3771265","name":"Umbrüggler Alm","link":"https://www.facebook.com/umbrueggleralm","bild":"images/umbrueggleralm.jpg","bildquelle":"https://www.almenrausch.at/uploads/tx_wctrip/header_5e89c4.jpg" 
+      "lat":"47.292361","lng":"11.379288","name":"Umbrüggler Alm","link":"https://www.facebook.com/umbrueggleralm","bild":"images/umbrueggleralm.jpg","bildquelle":"https://www.almenrausch.at/uploads/tx_wctrip/header_5e89c4.jpg" 
     },
     {
-      "lat":"47.3035458","lng":"11.3911077","name":"Bodensteinalm","link":"https://www.innsbruck.info/infrastruktur/detail/infrastruktur/bodenstein-alm-innsbruck.html","bild":"images/bodensteinalm.jpg","bildquelle":"https://www.innsbruck.info/emobilder/1000cx550c/30393/Bodensteinalm.jpg" 
+      "lat":"47.303215","lng":"11.387829","name":"Bodensteinalm","link":"https://www.innsbruck.info/infrastruktur/detail/infrastruktur/bodenstein-alm-innsbruck.html","bild":"images/bodensteinalm.jpg","bildquelle":"https://www.innsbruck.info/emobilder/1000cx550c/30393/Bodensteinalm.jpg" 
     },
     {
-      "lat":"47.298311","lng":"11.4039963","name":"Arzler Alm","link":"http://www.arzleralm.at/page.cfm?vpath=index","bild":"images/arzleralm.jpg","bildquelle":"http://www.arzleralm.at/data.cfm?vpath=teaserbilder/sommer-west2&dimensions=664x9999&cropmode=keepratio&noenlargement=no" 
+      "lat":"47.296513","lng":"11.403120","name":"Arzler Alm","link":"http://www.arzleralm.at/page.cfm?vpath=index","bild":"images/arzleralm.jpg","bildquelle":"http://www.arzleralm.at/data.cfm?vpath=teaserbilder/sommer-west2&dimensions=664x9999&cropmode=keepratio&noenlargement=no" 
     },
     {
       "lat":"47.297166","lng":"11.4177403","name":"Gasthaus Enzianhütte","link":"http://www.enzianhütte-rum.at/","bild":"images/enzianhuette.jpg","bildquelle":"https://www.karwendel-urlaub.de/fileadmin/user_upload/Enzianhuette_Innsbruck_Karwendel.JPG" 
