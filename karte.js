91bdf72d34f0f8dcae2dd0b5dc3cd19699b60f22
sagamol <christian.hofer@student.uibk.ac.at>
2018-06-09 14:53:03 +0200

    programm verschlankt und beschriftet

--- karte.js
+++ karte.js
@@ -1,30 +1,19 @@
+// eine neue Leaflet Karte definieren und mit der Fullscreen Funktion versehen
 let karte = L.map("map", {
     fullscreenControl: true,
 });
 
-//let bgdLayer = L.featureGroup();
-//let aldLayer = L.featureGroup();
+// zwei Layergruppen definieren und mit der markerCluster Funktion versehen
 const bgdLayer = L.markerClusterGroup().addTo(karte)
 const aldLayer = L.markerClusterGroup().addTo(karte)
 
+// Grundkartenlayer erstellen (OSM, basemapgrau, tiris_sommer, tiris_ortho)
 const myLayers = {
     osm: L.tileLayer(
         "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
             subdomains: ["a","b","c"],
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }
-    ),
-    geolandbasemap: L.tileLayer(
-        "https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
-        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
-        attribution: "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>",
-    }
-    ),
-    bmapoverlay: L.tileLayer(
-        "https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
-        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
-        attribution: "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>",
-    }
     ),
     eKarte_Tirol_Sommer: L.tileLayer(
         "http://wmts.kartetirol.at/wmts/gdi_base_summer/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80", {
@@ -48,18 +37,6 @@ const myLayers = {
         pane: "overlayPane",
     }
     ),
-    bmaphidpi: L.tileLayer(
-        "https://{s}.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg", {
-        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
-        attribution: "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>",
-    }
-    ),
-    bmaporthofoto30cm: L.tileLayer(
-        "https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg", {
-        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
-        attribution: "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>",
-    }
-    ),
 }
 
 // Layergruppen für die Elektronische Karte Tirol definieren
@@ -67,10 +44,7 @@ const tirisSommer = L.layerGroup([
     myLayers.eKarte_Tirol_Sommer,
     myLayers.gdi_nomenklatur
 ]);
-//const tirisWinter = L.layerGroup([
-  //  myLayers.eKarte_Tirol_Winter,
-  //  myLayers.gdi_nomenklatur
-//]);
+
 const tirisOrtho = L.layerGroup([
     myLayers.eKarte_Tirol_Ortho,
     myLayers.gdi_nomenklatur
@@ -79,28 +53,24 @@ const tirisOrtho = L.layerGroup([
 // Baselayer control für OSM, basemap.at, Elektronische Karte Tirol hinzufügen
 let karteControl = L.control.layers({
     "Openstreetmap": myLayers.osm,
-    "basemap.at Grundkarte": myLayers.geolandbasemap,
     "basemap.at grau": myLayers.bmapgrau,
-    "basemap.at highdpi": myLayers.bmaphidpi,
-    "basemap.at Orthofoto": myLayers.bmaporthofoto30cm,
     "Elektronische Karte Tirol - Sommer": tirisSommer,
     "Elektronische Karte Tirol - Orthophoto": tirisOrtho,
 }, {
     "Biergärten Innsbruck": bgdLayer,
     "Almen Nordkette bei Innsbruck": aldLayer,
-        //"Start / Ziel": overlayMarker,
     });
     karte.addControl(karteControl);
-    karte.addLayer(myLayers.geolandbasemap);
+    karte.addLayer(myLayers.osm);
     karte.setView([47.2688921, 11.3855037],13);
-
+    // Maßstabsleiste metrisch hinzufügen
     L.control.scale({           
         maxWidth : 200,        
         metric : true,          
         imperial : false,      
         position : "bottomleft" 
     }).addTo(karte);
-
+    // Marker aufgrund der Datenbank lat lngs hinzufügen und mit eigenem Icon versehen
     for (let bgd of biergartendata) {
         L.marker([bgd.lat, bgd.lng], {
             icon : L.icon ({
@@ -108,7 +78,7 @@ let karteControl = L.control.layers({
                 iconAnchor : [16,37],
                 popupAnchor : [0,-37],
             })
-        })
+        })// Popup mit Informationen aus der Datenbank alle in einem Layer
             .bindPopup(
             `<h1>${bgd.name}</h1>
             <p>${bgd.adresse}</p>
@@ -117,7 +87,6 @@ let karteControl = L.control.layers({
             <p>Bild entnommen aus: <a href='${bgd.bildquelle}'>Bildquelle Foto</a></p>`
         ).addTo(bgdLayer);
       }
-    // Frage nach bind.popups
     
     for (let ald of almendata) {
         L.marker([ald.lat, ald.lng], {
@@ -133,5 +102,5 @@ let karteControl = L.control.layers({
             <p>Bild entnommen aus: <a href='${ald.bildquelle}'>Bildquelle Foto</a></p>`
         ).addTo(aldLayer);
     };
-
+    // Kartengrenze festlegen
     karte.fitBounds(bgdLayer.getBounds());
