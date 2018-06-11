ba1fd9df07f3ea1d82956bc34e82abdf3159b781
sagamol <christian.hofer@student.uibk.ac.at>
2018-06-11 10:57:41 +0200

    umbrüggler fertig

--- umbrueggleralm.js
+++ umbrueggleralm.js
@@ -13,91 +13,54 @@ const myLayers = {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }
     ),
-    geolandbasemap: L.tileLayer(
-        "https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
-        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
-        attribution: "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>",
+    eKarte_Tirol_Sommer: L.tileLayer(
+        "http://wmts.kartetirol.at/wmts/gdi_base_summer/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80", {
+        attribution: "Datenquelle: <a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol'>eKarte Tirol</a>",
     }
     ),
-    bmapoverlay: L.tileLayer(
-        "https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
-        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
-        attribution: "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>",
+    eKarte_Tirol_Ortho: L.tileLayer(
+        "http://wmts.kartetirol.at/wmts/gdi_ortho/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80", {
+        attribution: "Datenquelle: <a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol'>eKarte Tirol</a>",
     }
     ),
-   // eKarte_Tirol_Sommer: L.tileLayer(
-     //   "http://wmts.kartetirol.at/wmts/gdi_base_summer/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80", {
-       // attribution: "Datenquelle: <a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol'>eKarte Tirol</a>",
-   // }
-   // ),
-   // eKarte_Tirol_Winter: L.tileLayer(
-   //     "http://wmts.kartetirol.at/wmts/gdi_base_winter/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80", {
-   //     attribution: "Datenquelle: <a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol'>eKarte Tirol</a>",
-   // }
-   // ),
-   // eKarte_Tirol_Ortho: L.tileLayer(
-    //    "http://wmts.kartetirol.at/wmts/gdi_ortho/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80", {
-    //    attribution: "Datenquelle: <a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol'>eKarte Tirol</a>",
-    //}
-    //),
     bmapgrau: L.tileLayer(
         "https://{s}.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png", {
         subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
         attribution: "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>",
     }
     ),
-    //gdi_nomenklatur: L.tileLayer(
-    //    "http://wmts.kartetirol.at/wmts/gdi_nomenklatur/GoogleMapsCompatible/{z}/{x}/{y}.png8", {
-    //        attribution: "Datenquelle: <a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol'>eKarte Tirol</a>",
-    //        pane: "overlayPane",
-    //}
-    //),
-    bmaphidpi: L.tileLayer(
-        "https://{s}.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg", {
-        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
-        attribution: "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>",
-    }
-    ),
-    bmaporthofoto30cm: L.tileLayer(
-        "https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg", {
-        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
-        attribution: "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>",
+    gdi_nomenklatur: L.tileLayer(
+        "http://wmts.kartetirol.at/wmts/gdi_nomenklatur/GoogleMapsCompatible/{z}/{x}/{y}.png8", {
+            attribution: "Datenquelle: <a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol'>eKarte Tirol</a>",
+            pane: "overlayPane",
     }
     ),
 }
 
 // Layergruppen für die Elektronische Karte Tirol definieren
-//const tirisSommer = L.layerGroup([
-  //  myLayers.eKarte_Tirol_Sommer,
-  //  myLayers.gdi_nomenklatur
-//]);
-//const tirisWinter = L.layerGroup([
-  //  myLayers.eKarte_Tirol_Winter,
-  //  myLayers.gdi_nomenklatur
-//]);
-//const tirisOrtho = L.layerGroup([
-  //  myLayers.eKarte_Tirol_Ortho,
-  //  myLayers.gdi_nomenklatur
-//]);
+const tirisSommer = L.layerGroup([
+    myLayers.eKarte_Tirol_Sommer,
+    myLayers.gdi_nomenklatur
+]);
+const tirisOrtho = L.layerGroup([
+    myLayers.eKarte_Tirol_Ortho,
+    myLayers.gdi_nomenklatur
+]);
 
 // Baselayer control für OSM, basemap.at, Elektronische Karte Tirol hinzufügen
 let karteControl = L.control.layers({
     "Openstreetmap": myLayers.osm,
-    "basemap.at Grundkarte": myLayers.geolandbasemap,
     "basemap.at grau": myLayers.bmapgrau,
-    "basemap.at highdpi": myLayers.bmaphidpi,
-    "basemap.at Orthofoto": myLayers.bmaporthofoto30cm,
-   // "Elektronische Karte Tirol - Sommer": tirisSommer,
-   // "Elektronische Karte Tirol - Winter": tirisWinter,
-   // "Elektronische Karte Tirol - Orthophoto": tirisOrtho,
+    "Elektronische Karte Tirol - Sommer": tirisSommer,
+    "Elektronische Karte Tirol - Orthophoto": tirisOrtho,
 }, {
         "Innsbruck - Umbrüggleralm": etappe12group,
         "Start / Ziel": overlayMarker,
     });
 
 karte.addControl(karteControl);
-karte.addLayer(myLayers.geolandbasemap);
-karte.setView([47.2688921, 11.3855037],12);
+karte.addLayer(myLayers.osm);
+karte.setView([47.2688921, 11.3855037],13);
 
 let hoehenprofil = L.control.elevation({
     position : "topright",
@@ -152,11 +115,6 @@ let gpxTrack2 = new L.GPX("data/umbrueggleralm2.gpx", {
 
 
 gpxTrack.on("loaded", function(evt) {
-    console.log("get_distance",evt.target.get_distance().toFixed(0))
-    console.log("get_elevation_min",evt.target.get_elevation_min().toFixed(0))
-    console.log("get_elevation_max",evt.target.get_elevation_max().toFixed(0))
-    console.log("get_elevation_gain",evt.target.get_elevation_gain().toFixed(0))
-    console.log("get_elevation_loss",evt.target.get_elevation_loss().toFixed(0))
     let laenge = evt.target.get_distance().toFixed(0);
     document.getElementById("laenge").innerHTML = laenge;
     let tiefster_Punkt = evt.target.get_elevation_min().toFixed(0);
@@ -228,6 +186,3 @@ L.marker([47.292205,11.379444],{
 ).addTo(overlayMarker);
 
 karte.fitBounds(etappe12group.getBounds());
-
-// GeoJSON Track als Linie in der Karte einzeichnen und auf Ausschnitt zoomen
-//let geojsonTrack = L.geoJSON(etappe12data).addTo(etappe12group);
